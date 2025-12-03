3dc57abdffcdf4699f3ea21d33adb1f6a8942be4
11sap <7illy7ile@gmail.com>
2025-12-03 20:23:00 +0300

    UPD, Chapter 6

--- chapters/chapter06.go
+++ chapters/chapter06.go
@@ -11,7 +11,9 @@ func Run06() {
 		ch6ex09, ch6ex10, ch6ex11, ch6ex12, ch6ex13, ch6ex14, ch6ex15, ch6ex16,
 		ch6ex17, ch6ex18, ch6ex19, ch6ex20, ch6ex21, ch6ex22, ch6ex23, ch6ex24,
 		ch6ex25, ch6ex26, ch6ex27, ch6ex28, ch6ex29, ch6ex30, ch6ex31, ch6ex32,
-		ch6ex33, ch6ex34, ch6ex35,
+		ch6ex33, ch6ex34, ch6ex35, ch6ex36, ch6ex37, ch6ex38, ch6ex39, ch6ex40,
+		ch6ex41, ch6ex42, ch6ex43, ch6ex44, ch6ex45, ch6ex46, ch6ex47, ch6ex48,
+		ch6ex49, ch6ex50, ch6ex51,
 	}
 
 	for {
@@ -705,4 +707,559 @@ func ch6ex35() {
 	fmt.Printf("г) Сумма цифр >5: %d\n", sumGreaterFive)
 	fmt.Printf("д) Произведение цифр >7: %d\n", productGreaterSeven)
 	fmt.Printf("е) Цифр 0 или 5: %d\n", countZeroOrFive)
+}
+
+func ch6ex36() {
+	fmt.Printf("Ex36. Цифры числа\n")
+	var n, a int
+	fmt.Scan(&n, &a)
+
+	countA := 0
+	sumGreaterA := 0
+	sumEven := 0
+	var x, y int
+	fmt.Scan(&x, &y)
+	countXY := 0
+
+	temp := n
+	for temp > 0 {
+		digit := temp % 10
+
+		if digit == a {
+			countA++
+		}
+
+		if digit > a {
+			sumGreaterA += digit
+		}
+
+		if digit%2 == 0 {
+			sumEven += digit
+		}
+
+		if digit == x || digit == y {
+			countXY++
+		}
+
+		temp /= 10
+	}
+
+	fmt.Printf("а) Цифра %d встречается: %d раз\n", a, countA)
+	fmt.Printf("б) Сумма цифр >%d: %d\n", a, sumGreaterA)
+	fmt.Printf("в) Сумма четных цифр: %d\n", sumEven)
+	fmt.Printf("г) Цифры %d и %d встречаются: %d раз\n", x, y, countXY)
+}
+
+func ch6ex37() {
+	fmt.Printf("Ex37. Номер цифры 8 от конца (самая левая)\n")
+	var n int
+	fmt.Scan(&n)
+
+	position := 1
+	result := 0
+	temp := n
+
+	for temp > 0 {
+		if temp%10 == 8 {
+			result = position
+		}
+		temp /= 10
+		position++
+	}
+
+	fmt.Printf("Номер цифры 8 от конца: %d\n", result)
+}
+
+func ch6ex38() {
+	fmt.Printf("Ex38. Номер цифры 3 от конца (самая правая)\n")
+	var n int
+	fmt.Scan(&n)
+
+	position := 1
+	result := 0
+	temp := n
+
+	for temp > 0 {
+		digit := temp % 10
+		if digit == 3 {
+			result = position
+			break
+		}
+		temp /= 10
+		position++
+	}
+
+	fmt.Printf("Номер цифры 3 от конца: %d\n", result)
+}
+
+func ch6ex39() {
+	fmt.Printf("Ex39. Цифры числа от первой\n")
+	var n int
+	fmt.Scan(&n)
+
+	digits := []int{}
+	temp := n
+	for temp > 0 {
+		digits = append(digits, temp%10)
+		temp /= 10
+	}
+
+	for i := len(digits) - 1; i >= 0; i-- {
+		fmt.Printf("%d\n", digits[i])
+	}
+}
+
+func ch6ex40() {
+	fmt.Printf("Ex40. Сколько раз встречается первая цифра\n")
+	var n int
+	fmt.Scan(&n)
+
+	temp := n
+	firstDigit := 0
+	for temp > 0 {
+		firstDigit = temp % 10
+		temp /= 10
+	}
+
+	count := 0
+	temp = n
+	for temp > 0 {
+		if temp%10 == firstDigit {
+			count++
+		}
+		temp /= 10
+	}
+
+	fmt.Printf("Первая цифра %d встречается: %d раз\n", firstDigit, count)
+}
+
+func ch6ex41() {
+	fmt.Printf("Ex41. Максимальная и минимальная цифры\n")
+	var n int
+	fmt.Scan(&n)
+
+	maxDigit := -1
+	minDigit := 10
+	temp := n
+
+	for temp > 0 {
+		digit := temp % 10
+		if digit > maxDigit {
+			maxDigit = digit
+		}
+		if digit < minDigit {
+			minDigit = digit
+		}
+		temp /= 10
+	}
+
+	fmt.Printf("а) Максимальная цифра: %d\n", maxDigit)
+	fmt.Printf("б) Минимальная цифра: %d\n", minDigit)
+}
+
+func ch6ex42() {
+	fmt.Printf("Ex42. Операции с max и min цифрами\n")
+	var n int
+	fmt.Scan(&n)
+
+	maxDigit := -1
+	minDigit := 10
+	temp := n
+
+	for temp > 0 {
+		digit := temp % 10
+		if digit > maxDigit {
+			maxDigit = digit
+		}
+		if digit < minDigit {
+			minDigit = digit
+		}
+		temp /= 10
+	}
+
+	difference := maxDigit - minDigit
+	sum := maxDigit + minDigit
+
+	fmt.Printf("а) Максимальная: %d, Минимальная: %d\n", maxDigit, minDigit)
+	fmt.Printf("б) Разность max-min: %d\n", difference)
+	fmt.Printf("в) Сумма max+min: %d\n", sum)
+}
+
+func ch6ex43() {
+	fmt.Printf("Ex43. Сумма max+min кратна ли a\n")
+	var n, a int
+	fmt.Scan(&n, &a)
+
+	maxDigit := -1
+	minDigit := 10
+	temp := n
+
+	for temp > 0 {
+		digit := temp % 10
+		if digit > maxDigit {
+			maxDigit = digit
+		}
+		if digit < minDigit {
+			minDigit = digit
+		}
+		temp /= 10
+	}
+
+	sum := maxDigit + minDigit
+	if sum%a == 0 {
+		fmt.Printf("Сумма %d+%d=%d кратна %d\n", maxDigit, minDigit, sum, a)
+	} else {
+		fmt.Printf("Сумма %d+%d=%d не кратна %d\n", maxDigit, minDigit, sum, a)
+	}
+}
+
+func ch6ex44() {
+	fmt.Printf("Ex44. Разность max-min четна ли\n")
+	var n int
+	fmt.Scan(&n)
+
+	maxDigit := -1
+	minDigit := 10
+	temp := n
+
+	for temp > 0 {
+		digit := temp % 10
+		if digit > maxDigit {
+			maxDigit = digit
+		}
+		if digit < minDigit {
+			minDigit = digit
+		}
+		temp /= 10
+	}
+
+	difference := maxDigit - minDigit
+	if difference%2 == 0 {
+		fmt.Printf("Разность %d-%d=%d четная\n", maxDigit, minDigit, difference)
+	} else {
+		fmt.Printf("Разность %d-%d=%d нечетная\n", maxDigit, minDigit, difference)
+	}
+}
+
+func ch6ex45() {
+	fmt.Printf("Ex45. Порядковые номера max и min (все цифры различны)\n")
+	var n int
+	fmt.Scan(&n)
+
+	maxDigit, minDigit := -1, 10
+	maxPosFromEnd, minPosFromEnd := 0, 0
+	maxPosFromStart, minPosFromStart := 0, 0
+
+	temp := n
+	positionFromEnd := 1
+	totalDigits := 0
+
+	temp2 := n
+	for temp2 > 0 {
+		totalDigits++
+		temp2 /= 10
+	}
+
+	temp = n
+	for temp > 0 {
+		digit := temp % 10
+
+		if digit > maxDigit {
+			maxDigit = digit
+			maxPosFromEnd = positionFromEnd
+			maxPosFromStart = totalDigits - positionFromEnd + 1
+		}
+
+		if digit < minDigit {
+			minDigit = digit
+			minPosFromEnd = positionFromEnd
+			minPosFromStart = totalDigits - positionFromEnd + 1
+		}
+
+		temp /= 10
+		positionFromEnd++
+	}
+
+	fmt.Printf("а) Максимальная цифра %d:\n", maxDigit)
+	fmt.Printf("   - от конца: позиция %d\n", maxPosFromEnd)
+	fmt.Printf("   - от начала: позиция %d\n", maxPosFromStart)
+
+	fmt.Printf("б) Минимальная цифра %d:\n", minDigit)
+	fmt.Printf("   - от конца: позиция %d\n", minPosFromEnd)
+	fmt.Printf("   - от начала: позиция %d\n", minPosFromStart)
+}
+
+func ch6ex46() {
+	fmt.Printf("Ex46. Порядковые номера max и min\n")
+	var n int
+	fmt.Scan(&n)
+
+	maxDigit, minDigit := -1, 10
+	maxPosFromEnd, minPosFromEnd := 0, 0
+	maxPosFromStart, minPosFromStart := 0, 0
+
+	temp := n
+	positionFromEnd := 1
+	totalDigits := 0
+
+	temp2 := n
+	for temp2 > 0 {
+		totalDigits++
+		temp2 /= 10
+	}
+
+	temp = n
+	for temp > 0 {
+		digit := temp % 10
+
+		if digit > maxDigit {
+			maxDigit = digit
+			maxPosFromEnd = positionFromEnd
+			maxPosFromStart = totalDigits - positionFromEnd + 1
+		}
+
+		if digit < minDigit {
+			minDigit = digit
+			minPosFromEnd = positionFromEnd
+			minPosFromStart = totalDigits - positionFromEnd + 1
+		}
+
+		temp /= 10
+		positionFromEnd++
+	}
+
+	fmt.Printf("а) От конца числа:\n")
+	fmt.Printf("   Максимальная цифра %d на позиции %d\n", maxDigit, maxPosFromEnd)
+	fmt.Printf("   Минимальная цифра %d на позиции %d\n", minDigit, minPosFromEnd)
+
+	fmt.Printf("б) От начала числа:\n")
+	fmt.Printf("   Максимальная цифра %d на позиции %d\n", maxDigit, maxPosFromStart)
+	fmt.Printf("   Минимальная цифра %d на позиции %d\n", minDigit, minPosFromStart)
+}
+
+func ch6ex47() {
+	fmt.Printf("Ex47. Что левее: max или min\n")
+	var n int
+	fmt.Scan(&n)
+
+	maxDigit, minDigit := -1, 10
+	maxPosFromStart, minPosFromStart := 0, 0
+
+	temp := n
+	positionFromEnd := 1
+	totalDigits := 0
+
+	temp2 := n
+	for temp2 > 0 {
+		totalDigits++
+		temp2 /= 10
+	}
+
+	temp = n
+	for temp > 0 {
+		digit := temp % 10
+		positionFromStart := totalDigits - positionFromEnd + 1
+
+		if digit > maxDigit {
+			maxDigit = digit
+			maxPosFromStart = positionFromStart
+		}
+
+		if digit < minDigit {
+			minDigit = digit
+			minPosFromStart = positionFromStart
+		}
+
+		temp /= 10
+		positionFromEnd++
+	}
+
+	if maxPosFromStart < minPosFromStart {
+		fmt.Printf("Максимальная цифра %d левее минимальной %d\n", maxDigit, minDigit)
+	} else {
+		fmt.Printf("Минимальная цифра %d левее максимальной %d\n", minDigit, maxDigit)
+	}
+}
+
+func ch6ex48() {
+	fmt.Printf("Ex48. Макс нечетная цифра и номер мин цифры слева\n")
+	var n int
+	fmt.Scan(&n)
+
+	maxOdd := -1
+	minDigit := 10
+	minPosFromStart := 0
+
+	temp := n
+	positionFromEnd := 1
+	totalDigits := 0
+
+	temp2 := n
+	for temp2 > 0 {
+		totalDigits++
+		temp2 /= 10
+	}
+
+	temp = n
+	for temp > 0 {
+		digit := temp % 10
+		positionFromStart := totalDigits - positionFromEnd + 1
+
+		if digit%2 == 1 && digit > maxOdd {
+			maxOdd = digit
+		}
+
+		if digit < minDigit {
+			minDigit = digit
+			minPosFromStart = positionFromStart
+		}
+
+		temp /= 10
+		positionFromEnd++
+	}
+
+	if maxOdd == -1 {
+		fmt.Printf("а) Нет нечетных цифр\n")
+	} else {
+		fmt.Printf("а) Максимальная нечетная цифра: %d\n", maxOdd)
+	}
+	fmt.Printf("б) Минимальная цифра %d на позиции %d (слева направо)\n", minDigit, minPosFromStart)
+}
+
+func ch6ex49() {
+	fmt.Printf("Ex49. Проверки числа\n")
+	var n, b int
+	fmt.Scan(&n, &b)
+
+	sum := 0
+	product := 1
+	count := 0
+	firstDigit := 0
+	lastDigit := n % 10
+
+	temp := n
+	for temp > 0 {
+		digit := temp % 10
+
+		sum += digit
+		product *= digit
+		count++
+
+		if temp < 10 {
+			firstDigit = temp
+		}
+
+		temp /= 10
+	}
+
+	fmt.Printf("а) Сумма цифр > %d: ", b)
+	fmt.Println(sum > b)
+
+	fmt.Printf("б) Произведение цифр < 50: ")
+	fmt.Println(product < 50)
+
+	fmt.Printf("в) Количество цифр четное: ")
+	fmt.Println(count%2 == 0)
+
+	fmt.Printf("г) Число четырехзначное: ")
+	fmt.Println(n >= 1000 && n <= 9999)
+
+	fmt.Printf("д) Первая цифра ≤ 5: ")
+	fmt.Println(firstDigit <= 5)
+
+	fmt.Printf("е) Начинается и заканчивается одной цифрой: ")
+	fmt.Println(firstDigit == lastDigit)
+
+	fmt.Printf("ж) Первая цифра > последней: ")
+	fmt.Println(firstDigit > lastDigit)
+}
+
+func ch6ex50() {
+	fmt.Printf("Ex50. Проверки числа 2\n")
+	var n, a, b, k int
+	fmt.Scan(&n, &a, &b, &k)
+
+	sum := 0
+	product := 1
+	count := 0
+	firstDigit := 0
+
+	temp := n
+	for temp > 0 {
+		digit := temp % 10
+
+		sum += digit
+		product *= digit
+		count++
+
+		if temp < 10 {
+			firstDigit = temp
+		}
+
+		temp /= 10
+	}
+
+	fmt.Printf("а) Сумма цифр < %d: ", a)
+	fmt.Println(sum < a)
+
+	fmt.Printf("б) Произведение цифр > %d: ", b)
+	fmt.Println(product > b)
+
+	fmt.Printf("в) Число %d-значное: ", k)
+	lower := 1
+	for i := 1; i < k; i++ {
+		lower *= 10
+	}
+	upper := lower * 10
+	fmt.Println(n >= lower && n < upper)
+
+	fmt.Printf("г) Первая цифра > %d: ", k)
+	fmt.Println(firstDigit > k)
+}
+
+func ch6ex51() {
+	fmt.Printf("Ex51. Комбинированные проверки\n")
+	var n, k, b, x, y, a, m int
+	fmt.Scan(&n, &k, &b, &x, &y, &a, &m)
+
+	sum := 0
+	product := 1
+	count := 0
+	firstDigit := 0
+	lastDigit := n % 10
+
+	temp := n
+	for temp > 0 {
+		digit := temp % 10
+
+		sum += digit
+		product *= digit
+		count++
+
+		if temp < 10 {
+			firstDigit = temp
+		}
+
+		temp /= 10
+	}
+
+	resultA := sum > k && n%2 == 0
+	fmt.Printf("а) Сумма цифр > %d и число четное: ", k)
+	fmt.Println(resultA)
+
+	resultB := count%2 == 0 && n <= b
+	fmt.Printf("б) Количество цифр четное и число ≤ %d: ", b)
+	fmt.Println(resultB)
+
+	resultC := firstDigit == x && lastDigit == y
+	fmt.Printf("в) Начинается на %d и заканчивается на %d: ", x, y)
+	fmt.Println(resultC)
+
+	resultD := product < a && n%b == 0
+	fmt.Printf("г) Произведение цифр < %d и число делится на %d: ", a, b)
+	fmt.Println(resultD)
+
+	resultE := sum > m && n%k == 0
+	fmt.Printf("д) Сумма цифр > %d и число делится на %d: ", m, k)
+	fmt.Println(resultE)
 }
